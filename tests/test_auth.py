ef139f0bde22e5d7eff76d8a35457ef2c902cd58
Goryaeva Kseniya <goryaeva_ks@mail.ru>
2025-03-30 17:42:10 +0300

    refactor value in test_auth.py ""

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,8 +1,3 @@
-from asyncio import timeout
-from shutil import ignore_patterns
-
-from selene import browser
-from selenium.common import TimeoutException
 
 from pages.for_tests import Authorization
 import allure
@@ -20,13 +15,13 @@ def test_auth(browser_manager):
         auth.click_button_enter()
 
     with allure.step('Ввести невалидный номер телефона'):
-        auth.type_mobile("58458456215")
+        auth.type_mobile('58458456215')
 
     with allure.step('Нажать на кнопку Получить код'):
         auth.click_button_det_code()
 
     with allure.step('Проверка текста ошибки: "Некорректный номер телефона"'):
-        auth.check_error_text_mobile("Некорректный номер телефона")
+        auth.check_error_text_mobile('Некорректный номер телефона')
 
 
 
