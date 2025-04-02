613e6184e52e33defba9e1272f7ffbce66f3ee1e
Goryaeva Kseniya <goryaeva_ks@mail.ru>
2025-04-02 18:50:21 +0300

    delete test_passed

--- tests/test_test.py
+++ tests/test_test.py
@@ -3,8 +3,5 @@ from selene import browser
 
 @allure.story('тесты для фейла')
 def test_passed():
-    with allure.step('Открываем главную страницу'):
-        browser.config.window_width = 1900
-        browser.config.window_height = 1080
-        browser.open("https://githubcom")
+    pass
 
