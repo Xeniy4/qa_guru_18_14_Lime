412fe07aba681f411b5b320496fb8060a34e3057
Goryaeva Kseniya <goryaevakss@gmail.com>
2025-04-05 14:19:31 +0300

    disable "enableVNC": True,

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,7 +24,7 @@ def selenoid():
         "browserName": "chrome",
         "browserVersion": "127.0",
         "selenoid:options": {
-            "enableVNC": True,
+            # "enableVNC": True,
             "enableVideo": True
         }
     }
@@ -52,6 +52,6 @@ def browser_manager():
     browser.config.base_url = "https://lime-shop.com/ru_ru"
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    browser.config.timeout = 200
+    browser.config.timeout = 120
     yield
     browser.quit()
