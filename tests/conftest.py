c153e0bbad9384278d882e34111aa13001ce50c4
Goryaeva Kseniya <goryaevakss@gmail.com>
2025-04-05 14:12:41 +0300

    add timeout 200

--- tests/conftest.py
+++ tests/conftest.py
@@ -52,6 +52,6 @@ def browser_manager():
     browser.config.base_url = "https://lime-shop.com/ru_ru"
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    browser.config.timeout = 120
+    browser.config.timeout = 200
     yield
     browser.quit()
