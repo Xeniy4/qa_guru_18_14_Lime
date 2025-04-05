8d911ca93d90d70f0c4141b13eb2151bbe638788
Goryaeva Kseniya <goryaevakss@gmail.com>
2025-04-05 17:38:09 +0300

    edit fixtures in config

--- tests/conftest.py
+++ tests/conftest.py
@@ -54,7 +54,7 @@ def browser_manager():
     browser.config.base_url = "https://lime-shop.com/ru_ru"
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    browser.config.timeout = 120
+    browser.config.timeout = 150
     yield
     browser.quit()
 
